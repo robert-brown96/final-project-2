04522c6ff89be4c15b1dd9eec0752ab116723c45
Robert <Robert@ROBERTS>
2017-03-12 10:18:22 -0500

    Add project files.

---
+++ FinalProject/Models/Saving.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using FinalProject.Models;
+
+
+
+namespace FinalProject.Models
+{
+    public class Saving
+    {
+        //account number primary key
+        [Display(Name="Account Number")]
+        public Int32 SavingID { get; set; }
+
+        //account name
+        [Required(ErrorMessage="Account name is required")]
+        public string Name { get; set; }
+
+        //calculated balance
+        //logic needed to recognize >5000 deposits
+        //actual balance will be computed through the saving transactions class
+        //set private backing variabl
+        private Decimal _decBalance;
+        //hopefully this works
+        public Decimal Balance
+        {
+            //normal get method
+            get { return _decBalance; }
+            //set method gets fucked
+            set
+            {
+                //iterate through list of objects
+                foreach(var SavingTransactions in SavingTransactions)
+                {
+                    //temp variable from saving transactions model
+                    decimal decAmount = SavingTransactions.Amount;
+                    //add to total balance
+                    _decBalance = _decBalance + decAmount;
+                }
+            }
+        }
+
+       
+        
+
+        //link to transactions
+        public virtual List<SavingTransactions> SavingTransactions { get; set; }
+
+    }
+}
---
+++ FinalProject/Models/SavingTransactions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class SavingTransactions
+    {
+
+        //transaction id
+        [Display(Name ="Transaction Number")]
+        [Key]
+        public Int32 SavingTransactionsID { get; set; }
+
+
+
+        //transaction date
+        [Display(Name ="Transaction Date")]
+        [Required(ErrorMessage =("A transaction date is required"))]
+        [DisplayFormat(DataFormatString ="{0:dd.MM.yyyy}",ApplyFormatInEditMode =true)]
+        public DateTime Date { get; set; }
+
+
+
+        [Required(ErrorMessage ="Enter an amount")]
+        public Decimal Amount { get; set; }
+
+        /*
+        public void Calc_Balance ()
+        {
+            Savings.Balance = Amount + Savings.Balance;
+        }
+
+        */
+        //link to the associated account
+        public virtual Saving Savings { get; set; }
+
+        //other comments?
+        public string Comments { get; set; }
+
+
+
+
+
+    }
+}
