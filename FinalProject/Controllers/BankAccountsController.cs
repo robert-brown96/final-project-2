d16175baad4ea7b3006aef3df976044ce228926e
Robert <Rbrown19@utexas.edu>
2017-03-29 18:19:56 -0500

    end of 3/29

---
+++ FinalProject/Controllers/BankAccountsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.DAL;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class BankAccountsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: BankAccounts
+        public ActionResult Index()
+        {
+            List<BankAccount> AccountList = new List<BankAccount>();
+            AccountList = db.Accounts.ToList();
+            return View(AccountList);
+        }
+
+        // GET: BankAccounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BankAccount bankAccount = db.Accounts.Find(id);
+            if (bankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // GET: BankAccounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BankAccounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BankAccountID,AccountNumber,Name,Balance")] BankAccount bankAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(bankAccount);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bankAccount);
+        }
+
+        // GET: BankAccounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BankAccount bankAccount = db.Accounts.Find(id);
+            if (bankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // POST: BankAccounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BankAccountID,AccountNumber,Name,Balance")] BankAccount bankAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bankAccount).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bankAccount);
+        }
+
+        // GET: BankAccounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BankAccount bankAccount = db.Accounts.Find(id);
+            if (bankAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bankAccount);
+        }
+
+        // POST: BankAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BankAccount bankAccount = db.Accounts.Find(id);
+            db.Accounts.Remove(bankAccount);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
