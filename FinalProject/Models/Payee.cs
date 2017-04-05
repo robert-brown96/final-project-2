3e5350e948e49d554b8f2dda9c3c4810d5012356
Robert <Rbrown19@utexas.edu>
2017-04-05 16:50:57 -0500

    payee and portfolio

---
+++ FinalProject/Models/Payee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using FinalProject.Utilities;
+
+namespace FinalProject.Models
+{
+    public class Payee
+    {
+
+        public Int32 PayeeID { get; set; }
+
+        public string PayeeType { get; set; }
+
+        public string Address { get; set; }
+
+        public 
+    }
+}
---
+++ FinalProject/Models/StockPortfolio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using FinalProject.Utilities;
+using FinalProject.DAL;
+
+namespace FinalProject.Models
+{
+    public class StockPortfolio
+    {
+
+        //primary key
+        public Int32 StockPortfolioID { get; set; }
+
+
+        //cash value
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public Decimal CashBalance { get; set; }
+
+
+        //account number for display
+        [Display(Name = "Account Number")]
+        public Int32 AccountNumber { get; set; }
+
+        //reference to user
+        public virtual AppUser User { get; set; }
+    }
+}
