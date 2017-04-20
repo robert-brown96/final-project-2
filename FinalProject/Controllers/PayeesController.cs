eda699d736583342e4f447035f793c69cf1a31da
ewarr <ewarr@DESKTOP-MO38K71>
2017-04-19 21:13:32 -0500

    CommentPayeeController

--- FinalProject/Controllers/PayeesController.cs
+++ FinalProject/Controllers/PayeesController.cs
@@ -1,149 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-using FinalProject.DAL;
-using FinalProject.Models;
-using Microsoft.AspNet.Identity;
+//using System.Data;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Web;
+//using System.Web.Mvc;
+//using FinalProject.DAL;
+//using FinalProject.Models;
+//using Microsoft.AspNet.Identity;
 
-namespace FinalProject.Controllers
-{
-    public class PayeesController : Controller
-    {
-        private AppDbContext db = new AppDbContext();
+//namespace FinalProject.Controllers
+//{
+    //public class PayeesController : Controller
+    //{
+    //    private AppDbContext db = new AppDbContext();
 
-        // GET: Payees
-        public ActionResult Index()
-        {
-            return View(db.Payees.ToList());
-        }
+    //    // GET: Payees
+    //    public ActionResult Index()
+    //    {
+    //        return View(db.Payees.ToList());
+    //    }
 
-        // GET: Payees/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Payee payee = db.Payees.Find(id);
-            if (payee == null)
-            {
-                return HttpNotFound();
-            }
-            return View(payee);
-        }
+    //    // GET: Payees/Details/5
+    //    public ActionResult Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Payee payee = db.Payees.Find(id);
+    //        if (payee == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(payee);
+    //    }
 
-        // GET: Payees/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+    //    // GET: Payees/Create
+    //    public ActionResult Create()
+    //    {
+    //        return View();
+    //    }
 
-        // POST: Payees/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PayeeID,PayeeName,PayeeType,Address")] Payee payee)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Payees.Add(payee);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+    //    // POST: Payees/Create
+    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create([Bind(Include = "PayeeID,PayeeName,PayeeType,Address")] Payee payee)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Payees.Add(payee);
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
 
-            return View(payee);
-        }
+    //        return View(payee);
+    //    }
 
-        // GET: Payees/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Payee payee = db.Payees.Find(id);
-            if (payee == null)
-            {
-                return HttpNotFound();
-            }
-            return View(payee);
-        }
+    //    // GET: Payees/Edit/5
+    //    public ActionResult Edit(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Payee payee = db.Payees.Find(id);
+    //        if (payee == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(payee);
+    //    }
 
-        // POST: Payees/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PayeeID,PayeeName,PayeeType,Address")] Payee payee)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(payee).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(payee);
-        }
+    //    // POST: Payees/Edit/5
+    //    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit([Bind(Include = "PayeeID,PayeeName,PayeeType,Address")] Payee payee)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Entry(payee).State = EntityState.Modified;
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+    //        return View(payee);
+    //    }
 
-        // GET: Payees/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Payee payee = db.Payees.Find(id);
-            if (payee == null)
-            {
-                return HttpNotFound();
-            }
-            return View(payee);
-        }
+    //    // GET: Payees/Delete/5
+    //    public ActionResult Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Payee payee = db.Payees.Find(id);
+    //        if (payee == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(payee);
+    //    }
 
-        // POST: Payees/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Payee payee = db.Payees.Find(id);
-            db.Payees.Remove(payee);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+    //    // POST: Payees/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult DeleteConfirmed(int id)
+    //    {
+    //        Payee payee = db.Payees.Find(id);
+    //        db.Payees.Remove(payee);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
 
-        public SelectList GetAllPayees(overload)
-        {
-        }
+    //    public SelectList GetAllPayees(overload)
+    //    {
+    //    }
 
-        public SelectList GetAllPayees()
-        {
-        }
+    //    public SelectList GetAllPayees()
+    //    {
+    //    }
 
-        public SelectList GetPayees()
-        {
-            AppUser currentuser;
-            currentuser = db.Users.Find(User.Identity.GetUserId());
-            SelectList allpayments = new SelectList(currentuser.Payments, "PaymentID", "Payee.Name");
-            return allpayments;
-        }
-        //NOTE: THIS CODE WAS ADDED
-
-
+    //    public SelectList GetPayees()
+    //    {
+    //        AppUser currentuser;
+    //        currentuser = db.Users.Find(User.Identity.GetUserId());
+    //        SelectList allpayments = new SelectList(currentuser.Payments, "PaymentID", "Payee.Name");
+    //        return allpayments;
+    //    }
+    //    //NOTE: THIS CODE WAS ADDED
 
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
-}
+
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+//        //    base.Dispose(disposing);
+//        //}
+//    }
+//}
